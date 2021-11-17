86735e973f69db84b4e96c1803a346bad52b141c
Jose <josemanuelalonsopozas@gmail.com>
2021-11-17 09:50:30 +0100

    Commit 5 metodos getter y setter para todos los atributos

--- Ordenador.java
+++ Ordenador.java
@@ -14,4 +14,36 @@ public class Ordenador{
         encendidoPC = true;  
         ventilador = nuevoVentilador;
     }
+    
+    public String getMarca() {
+        return marca;
+    }
+    
+    public void setMarca(String marcaOrdenador){
+        marca = marcaOrdenador;
+    }
+    
+    public int getHorasEncendido() {
+        return horasEncendido;
+    }
+    
+    public void setHorasEncendido(int horasEncendidoOrdenador){
+        horasEncendido = horasEncendidoOrdenador;
+    }
+    
+    public boolean getEstado() {
+        return encendidoPC;
+    }
+    
+    public void setEncendidoPC(boolean encenderPC){
+        encendidoPC = encenderPC;
+    }
+    
+    public Ventilador getVentilador() {
+        return ventilador;
+    }
+    
+    public void setVentilador(Ventilador nuevoVentilador){
+        ventilador = nuevoVentilador;
+    }
 }
